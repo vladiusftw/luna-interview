0994a57442210a11e657c2ebf5b70f737fc2d42a
Abdul Rahman Al Sbeinaty <alsbeinaty@gmail.com>
2022-11-09 20:46:56 +0400

    update

--- src/Components/ExploreCaroussel.jsx
+++ src/Components/ExploreCaroussel.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import { Stories } from "../Stories";
 import Grid from "@mui/material/Grid";
 import Arrows from "./Arrows";
@@ -29,42 +28,10 @@ const ExploreCaroussel = () => {
     setCounter(temp);
   };
 
-  const Item = ({ item, index }) => {
+  const Item = ({ item }) => {
     return (
       <Box className="explore-item-container">
         <img src={item.image} className="explore-item-img" />
-        {index == 0 || index == 5 ? (
-          <Box>
-            <Typography className="explore-item-title" variant="p">
-              {item.title}
-            </Typography>
-            <Box className="explore-item-icons-container">
-              <Box className="explore-item-loc-container">
-                <img
-                  className="explore-item-loc-img"
-                  src={require("../assets/pin.png")}
-                />
-                <Typography className="explore-item-loc-text" variant="p">
-                  {item.location}
-                </Typography>
-              </Box>
-              <Box className="explore-item-date-container">
-                <img
-                  className="explore-item-date-img"
-                  src={require("../assets/calendar.png")}
-                />
-                <Typography className="explore-item-date-text" variant="p">
-                  {item.date}
-                </Typography>
-              </Box>
-            </Box>
-            <Typography className="explore-item-content" variant="p">
-              {item.content}
-            </Typography>
-          </Box>
-        ) : (
-          <></>
-        )}
       </Box>
     );
   };
